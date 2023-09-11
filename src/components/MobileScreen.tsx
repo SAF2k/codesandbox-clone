e61bd711502aab963f23f5aa32330e21906135ed
SAF2k <safwathkhan2@gmail.com>
2023-09-11 23:51:51 +0530

    compatibily to md

--- src/components/MobileScreen.tsx
+++ src/components/MobileScreen.tsx
@@ -5,7 +5,7 @@ const MobileScreen = () => {
     <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-[radial-gradient(circle_farthest-side_at_calc(300px)_calc(300px),_var(--color-secondary)_0%,_transparent_100%)] md:hidden">
       {/* This is a very ugly way of adding such a message, don't build it like this :) */}
       <p className="px-10 text-center text-xl text-white">
-        Sorry, I only made this page work on desktop. Perhaps in a future video
+        Sorry, I only made this page work on desktop. Perhaps in a future update
         we can make this responsive as well 😁
         <br />
         <Link
